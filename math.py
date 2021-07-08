2684e1a5e9ffad89567b36a8d42b6f85e1e501c6
MrDemon-github <78741321+MrDemon-github@users.noreply.github.com>
2021-07-08 14:28:45 +0530

    initial commit on math.py

---
+++ math.py
@@ -0,0 +1,15 @@
+#import flask
+def add(x,y): 
+		pass
+def subtract(x,y): 
+	 	return x-y #substraction by master
+def multiply(x,y):
+ 		return x*y #implemented mul() by bug456
+def divide(x,y): 
+		if y==0 #implemneted div() by bug789
+		return "bivide  by zero"
+		else
+#square() implimentation
+def square(x)
+		pass
+
