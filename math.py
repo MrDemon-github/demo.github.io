deab18519f35ca3653b5000f7580c5fa35ede5c2
Dhanraj_Shinde <dhanrajshinde06@gmail.com>
2021-07-08 14:37:16 +0530

    added header to methods

--- math.py
+++ math.py
@@ -1,10 +1,14 @@
 #import flask
+#add() imp
 def add(x,y): 
 		pass
 def subtract(x,y): 
+	#sub() imp
 	 	return x-y #substraction by master
+	 	#mul() implimented
 def multiply(x,y):
  		return x*y #implemented mul() by bug456
+ 		#div() imp
 def divide(x,y): 
 		if y==0 #implemneted div() by bug789
 		return "bivide  by zero"
